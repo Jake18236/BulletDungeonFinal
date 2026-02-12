7a947632440a93fab26e8c805dcf97053481dadb
Jake18236 <144159145+Jake18236@users.noreply.github.com>
2026-02-12 03:41:59 +0000

    h

--- client/src/components/SpriteProps.tsx
+++ client/src/components/SpriteProps.tsx
@@ -226,7 +226,7 @@ export const bossLaserSpriteSheet = (() => {
 
 export const bossLaserContinueSprite = (() => {
   const img = new Image();
-  img.src = "/sprites/enemy/boss-laser-continue.png";
+  img.src = "/sprites/enemy/boss-laser-continued.png";
   return img;
 })();
 
